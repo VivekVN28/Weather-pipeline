2ba22effce7a6c656082e46cbbdbd5c8b7b100ed
VivekV-N <vivekvnambiar2004@gmail.com>
2026-01-21 23:55:23 +0530

    Successfully created postgres table and inserted from weatherstack

--- weather-data-project/api_request.py
+++ weather-data-project/api_request.py
@@ -7,10 +7,11 @@ def fetch_data():
         response=requests.get(api_url)
         response.raise_for_status()
         print("API response received")
+        print(response.json)
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         raise
-data=None
+data=fetch_data()
 def mock_fetch_data():
-    return {'request': {'type': 'City', 'query': 'New York, United States of America', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'New York', 'country': 'United States of America', 'region': 'New York', 'lat': '40.714', 'lon': '-74.006', 'timezone_id': 'America/New_York', 'localtime': '2026-01-20 12:42', 'localtime_epoch': 1768912920, 'utc_offset': '-5.0'}, 'current': {'observation_time': '05:42 PM', 'temperature': -7, 'weather_code': 113, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png'], 'weather_descriptions': ['Sunny'], 'astro': {'sunrise': '07:16 AM', 'sunset': '04:59 PM', 'moonrise': '08:29 AM', 'moonset': '07:01 PM', 'moon_phase': 'Waxing Crescent', 'moon_illumination': 1}, 'air_quality': {'co': '263.85', 'no2': '25.35', 'o3': '46', 'so2': '6.15', 'pm2_5': '9.65', 'pm10': '9.85', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 23, 'wind_degree': 270, 'wind_dir': 'W', 'pressure': 1027, 'precip': 0, 'humidity': 33, 'cloudcover': 0, 'feelslike': -15, 'uv_index': 2, 'visibility': 16, 'is_day': 'yes'}}
+    return data
--- weather-data-project/insert_records.py
+++ weather-data-project/insert_records.py
@@ -1,3 +1,78 @@
 from api_request import mock_fetch_data
+import psycopg2
 
-print(mock_fetch_data())
+def connect_to_db():
+    print("connecting to the PostgresSQL")
+    try:
+        conn= psycopg2.connect(
+            host="localhost",
+            port=5432,
+            dbname='db',
+            user="db_user",
+            password="db_password"
+        )
+        return conn
+    except psycopg2.Error as e:
+        print(f"Database connection failed: {e}")
+        raise
+
+def create_table(conn):
+    print("Creating table if not exist..")
+    try:
+        cursor=conn.cursor()
+        cursor.execute("""
+                    CREATE SCHEMA IF NOT EXISTS dev;
+                    CREATE TABLE IF NOT EXISTS dev.raw_weather_data(
+                       id SERIAL PRIMARY KEY,
+                       city TEXT,
+                       temperature FLOAT,
+                       weather_descriptions TEXT,
+                       wind_speed FLOAT,
+                       time TIMESTAMP,
+                       insert_at TIMESTAMP,
+                       ufc_offset TEXT
+                       );
+                       """)
+        conn.commit()
+        print("Table was created")
+    except psycopg2.Error as e:
+        print(f"Failed to create a table")
+        raise
+
+def insert_records(conn,data):
+   
+
+    try:
+        
+        print(f"Inserting weather data into the database...")
+    
+        weather=data['current']
+        location=data['location']
+        cursor=conn.cursor()
+        cursor.execute("""
+                INSERT INTO dev.raw_weather_data (
+                    city,
+                    temperature,
+                    weather_descriptions,
+                    wind_speed,
+                    time,
+                    insert_at,
+                    ufc_offset
+                ) VALUES (%s,%s,%s,%s,%s,NOW(),%s)
+            """,(
+                location['name'],
+                weather['temperature'],
+                weather['weather_descriptions'][0],
+                weather['wind_speed'],
+                location['localtime'],
+                location['utc_offset']
+            ))
+        conn.commit()
+        print("Data successfully inserted")
+    except psycopg2.Error as e:
+        print(f"Error inserting data into the table:{e}")
+        raise
+data=mock_fetch_data()
+conn=connect_to_db()
+create_table(conn)
+insert_records(conn,data)
